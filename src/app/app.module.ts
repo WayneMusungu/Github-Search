4118d87f8a0ca8a8ccbcf073cd84e866f60b25c1
WayneMusungu <waynemuyera17@gmail.com>
2022-04-16 13:54:27 +0300

    Add a pipe date count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { DateCountPipe } from './date-count.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
+    DateCountPipe,
 
   ],
   imports: [
